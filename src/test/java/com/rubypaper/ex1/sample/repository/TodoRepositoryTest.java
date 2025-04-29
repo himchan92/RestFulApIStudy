c798dd7c678865e5e078a8484950c413177c04a6
omea92 <thdjun92@naver.com>
2025-04-30 01:12:17 +0900

    Spring Data Jpa update select delete test

--- src/test/java/com/rubypaper/ex1/sample/repository/TodoRepositoryTest.java
+++ src/test/java/com/rubypaper/ex1/sample/repository/TodoRepositoryTest.java
@@ -2,11 +2,13 @@ package com.rubypaper.ex1.sample.repository;
 
 import com.rubypaper.ex1.sample.dto.TodoEntity;
 import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,5 +45,61 @@ class TodoRepositoryTest {
             todoRepository.save(todoEntity);
             System.out.println("New TodoEntity MNO: " + todoEntity.getMno());
         }
+    }
+
+    @Test
+    public void testRead() {
+        Long mno = 58L;
+
+        Optional<TodoEntity> result = todoRepository.findById(mno);
+
+        //Optional 제공 존재유무 체크
+        result.ifPresent(todoEntity -> {
+            System.out.println(todoEntity);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void testRead2() {
+        Long mno = 55L;
+
+        Optional<TodoEntity> result2 = todoRepository.findById(mno);
+
+        //Optional 제공 존재유무 체크
+        result2.ifPresent(todoEntity -> {
+            System.out.println(todoEntity);
+        });
+    }
+
+    @Test
+    @Transactional
+    @Commit //트랜젝션내에서는 수행 후 자동 롤백으로 반영결과 보고싶은경우(운영에서는 비추)
+    public void testUpdateDirtyCheck() {
+        Long mno = 58L;
+
+        Optional<TodoEntity> result = todoRepository.findById(mno);
+
+        TodoEntity todoEntity = result.get();
+
+        System.out.println("OLD: " + todoEntity);
+
+        todoEntity.changeTitle("Change Title..." + Math.random());
+        todoEntity.changeWriter("Change Writer..." + Math.random());
+
+        System.out.println("Changed : " + todoEntity);
+    }
+
+    @Test
+    @Transactional
+    @Commit
+    public void testDelete() {
+        Long mno = 101L;
+
+        Optional<TodoEntity> result = todoRepository.findById(mno);
+
+        result.ifPresent(todoEntity -> {
+            todoRepository.delete(todoEntity);
+        });
     }
 }
