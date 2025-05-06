d3c14029febf4cf08adf2ee7f4a84b0f032fe156
omea92 <thdjun92@naver.com>
2025-05-06 18:48:10 +0900

    init commit

--- src/main/java/com/rubypaper/ex1/sample/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rubypaper.ex1.sample.repository;
-
-import com.rubypaper.ex1.sample.dto.TodoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// JPA CRUD + Paging
-public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-
-}
--- src/main/java/com/rubypaper/ex1/sample/service/SampleService.java
+++
@@ -1,8 +0,0 @@
-package com.rubypaper.ex1.sample.service;
-
-import org.springframework.stereotype.Service;
-
-@Service //스프링 빈 등록
-public class SampleService {
-
-}
