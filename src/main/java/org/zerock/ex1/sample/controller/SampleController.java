ad0534eb4bc65c1743c48aef2404c7ff990db85a
himchan92 <omea92@naver.com>
2025-06-11 12:30:15 +0900

    ex1 controller api test

--- src/main/java/org/zerock/ex1/sample/controller/SampleController.java
+++ src/main/java/org/zerock/ex1/sample/controller/SampleController.java
@@ -1,13 +1,18 @@
 package org.zerock.ex1.sample.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.ex1.sample.service.SampleService;
 
 @Log4j2
 @RestController
 @RequestMapping("/api/v1/sample")
+@RequiredArgsConstructor //final 대상 생성자 DI 지원
 public class SampleController {
+
+    private final SampleService sampleService;
 
     @RequestMapping("/hello")
     public String[] hello() {
