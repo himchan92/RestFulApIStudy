10fc77f8466880c4028d58727d442231167cdd53
himchan92 <omea92@naver.com>
2025-06-11 12:22:23 +0900

    ex1 init commit

---
+++ src/main/java/org/zerock/ex1/sample/dto/SampleDTO.java
@@ -0,0 +1,16 @@
+package org.zerock.ex1.sample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleDTO {
+
+    private Long ssn;
+    private String name;
+}
---
+++ src/main/java/org/zerock/ex1/sample/service/SampleService.java
@@ -0,0 +1,8 @@
+package org.zerock.ex1.sample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service //스프링 빈생성, 컨테이너관리
+public class SampleService {
+
+}
