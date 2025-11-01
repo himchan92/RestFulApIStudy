5c5470a02ea2000a380c3bd788659514afb3e532
user <thdjun92@naver.com>
2025-11-01 16:08:21 +0900

    security 수정

--- src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
@@ -3,6 +3,7 @@ package org.zerock.ex3.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class CustomSecurityConfig {
 
     @Bean
