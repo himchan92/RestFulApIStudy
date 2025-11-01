cf22b6decc1846b4b5e6b2bcf450e936bb2b9a1e
user <thdjun92@naver.com>
2025-11-01 15:16:00 +0900

    test code

--- src/test/java/org/zerock/ex3/member/repository/MemberRepositoryTest.java
+++ src/test/java/org/zerock/ex3/member/repository/MemberRepositoryTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex3.member.entity.MemberEntity;
+import org.zerock.ex3.member.exception.MemberExceptions;
+
+import java.util.Optional;
 
 @SpringBootTest
 class MemberRepositoryTest {
@@ -28,5 +33,42 @@ class MemberRepositoryTest {
 
             memberRepository.save(memberEntity);
         }
+    }
+
+    @Test
+    public void testRead() {
+        String mid = "user1";
+
+        Optional<MemberEntity> result = memberRepository.findById(mid);
+
+        MemberEntity memberEntity = result.orElseThrow(MemberExceptions.NOT_FOUND::get);
+
+        System.out.println(memberEntity);
+    }
+
+    @Test
+    @Transactional
+    @Commit
+    public void testUpdate() {
+        String mid = "user1";
+
+        Optional<MemberEntity> result = memberRepository.findById(mid);
+
+        MemberEntity memberEntity = result.orElseThrow(MemberExceptions.NOT_FOUND::get);
+
+        memberEntity.changePassword(passwordEncoder.encode("2222"));
+        memberEntity.changeName("USER1-1");
+    }
+
+    @Test
+    @Transactional
+    @Commit
+    public void testDelete() {
+        String mid = "user1";
+
+        Optional<MemberEntity> result = memberRepository.findById(mid);
+        MemberEntity memberEntity = result.orElseThrow(() -> MemberExceptions.NOT_FOUND.get());
+
+        memberRepository.delete(memberEntity);
     }
 }
