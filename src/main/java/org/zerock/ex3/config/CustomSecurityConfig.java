6f2b6b5550880129fe6fdded867d35eab2b3214e
user <thdjun92@naver.com>
2025-11-01 15:24:29 +0900

    security filter setting

--- src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/ex3/config/CustomSecurityConfig.java
@@ -3,12 +3,33 @@ package org.zerock.ex3.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
 public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        log.info("filter chain..................");
+
+        httpSecurity.formLogin(httpSecurityFormLoginConfigurer -> {
+            httpSecurityFormLoginConfigurer.disable();
+        });
+
+        httpSecurity.logout(config -> config.disable());
+        httpSecurity.csrf(config -> {config.disable();});
+
+        httpSecurity.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+            httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        });
+
+        return httpSecurity.build();
+    }
 
     //TODO: 비밀번호 암호화 설정
     @Bean
